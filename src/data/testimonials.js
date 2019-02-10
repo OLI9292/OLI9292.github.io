3130d10405ed70860cad99a908c6306b6f083c53
Oliver Plunkett <otplunkett@gmail.com>
2019-02-10 14:16:11 -0500

    testimonails

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -16,7 +16,7 @@ module.exports = [
   },
   {
     text:
-      "I have had a number of personal trainers i've worked with and I can say that Jay's style has been very effective so far. He manages to be motivational while still maintaining an accurate awareness of my limitations. I can tell he is not unrealistic with where he pushes clients. Overall, my experience has been positive and plan to continue working with Jay -- I would recommend him to friends of all levels of fitness experience; athlete or beginner.",
+      "I have had a number of personal trainers I've worked with and I can say that Jay's style has been very effective so far. He manages to be motivational while still maintaining an accurate awareness of my limitations. I can tell he is not unrealistic with where he pushes clients. Overall, my experience has been positive and plan to continue working with Jay -- I would recommend him to friends of all levels of fitness experience; athlete or beginner.",
     from: 'Rick Welch',
   },
   {
@@ -28,5 +28,16 @@ module.exports = [
     text:
       "Jay Mark has taught me so much over the 2 years of knowing him, I'm forever grateful. So grateful to the point that anytime I receive a compliment on my striking skills, I hand it to him. Jay has been so influential to me, I’ve learned so much about Muay Thai & striking in general due to his teachings. It means so much more to me that he taught my first lesson in kickboxing. He took me from not knowing the tiny basics to having the muscle memory and speed that I currently do. It's all owed to him, his patience and drive is so inspirational, a champ, displaying what a REAL COACH embodies. He’s not only my mentor and coach, he’s a friend and the big bro I have never had, and always wished for. Reign on king.",
     from: 'Gregory Dowdy',
+  },
+  {
+    text:
+      'From my very first session with Jay Mark, I realized what I was missing all those years “boxing”. Each session with Jay Mark is not only physically, but mentally challenging as well. As a professional fighter, Jay Mark is skilled in Muai Thai fighting and technique. In additions, he has years of experience in general fitness and personal training, a rare combination to find in a trainer.  Over the years there hasn’t been a question he hasn’t able to answer intelligently (and I ask a lot of questions 🙈) or a move he wasn’t able to explain and help me master. On a weekly basis he challenges my fitness level, learning and growing each week.',
+    from: 'Raquel Ghadamian',
+  },
+  ,
+  {
+    text:
+      'Jay has trained me in muay thai and kickboxing for 2 years; and in that time has given me the confidence and ability to compete and win in amateur competitions.  Jay knows how to bring out the best in you, while always keeping a smile.  One of the things I appreciate most about Jay is how he embodies everything he teaches: confidence, humility, and work ethic.  Thanks Jay!',
+    from: 'Oliver Plunkett',
   },
 ]
